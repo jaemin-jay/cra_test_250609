cfe8e5df9dd9890b794f6446659ecf90f21a2d6c
eunho.lee <eno99.lee@samsung.com>
2025-06-09 10:19:01 +0900

    make function for printting each type-option
    Signed-off-by: eunho.lee <eno99.lee@samsung.com>

--- main.cpp
+++ main.cpp
@@ -75,6 +75,61 @@ void delay(int ms)
     }
 }
 
+void print_car_option()
+{
+    printf(CLEAR_SCREEN);
+
+    printf("        ______________\n");
+    printf("       /|            | \n");
+    printf("  ____/_|_____________|____\n");
+    printf(" |                      O  |\n");
+    printf(" '-(@)----------------(@)--'\n");
+    printf("===============================\n");
+    printf("어떤 차량 타입을 선택할까요?\n");
+    printf("1. Sedan\n");
+    printf("2. SUV\n");
+    printf("3. Truck\n");
+}
+
+void print_brake_system_option()
+{
+    printf(CLEAR_SCREEN);
+    printf("어떤 제동장치를 선택할까요?\n");
+    printf("0. 뒤로가기\n");
+    printf("1. MANDO\n");
+    printf("2. CONTINENTAL\n");
+    printf("3. BOSCH\n");
+}
+
+void print_engine_option()
+{
+    printf(CLEAR_SCREEN);
+    printf("어떤 엔진을 탑재할까요?\n");
+    printf("0. 뒤로가기\n");
+    printf("1. GM\n");
+    printf("2. TOYOTA\n");
+    printf("3. WIA\n");
+    printf("4. 고장난 엔진\n");
+}
+
+void print_steer_system_option()
+{
+    printf(CLEAR_SCREEN);
+    printf("어떤 조향장치를 선택할까요?\n");
+    printf("0. 뒤로가기\n");
+    printf("1. BOSCH\n");
+    printf("2. MOBIS\n");
+}
+
+void print_run_test()
+{
+    printf(CLEAR_SCREEN);
+    printf("멋진 차량이 완성되었습니다.\n");
+    printf("어떤 동작을 할까요?\n");
+    printf("0. 처음 화면으로 돌아가기\n");
+    printf("1. RUN\n");
+    printf("2. Test\n");
+}
 int main()
 {
     char buf[100];
@@ -84,56 +139,16 @@ int main()
     {
 
         if (step == CarType_Q)
-        {
-            printf(CLEAR_SCREEN);
-
-            printf("        ______________\n");
-            printf("       /|            | \n");
-            printf("  ____/_|_____________|____\n");
-            printf(" |                      O  |\n");
-            printf(" '-(@)----------------(@)--'\n");
-            printf("===============================\n");
-            printf("어떤 차량 타입을 선택할까요?\n");
-            printf("1. Sedan\n");
-            printf("2. SUV\n");
-            printf("3. Truck\n");
-        }
+            print_car_option();
         else if (step == Engine_Q)
-        {
-            printf(CLEAR_SCREEN);
-            printf("어떤 엔진을 탑재할까요?\n");
-            printf("0. 뒤로가기\n");
-            printf("1. GM\n");
-            printf("2. TOYOTA\n");
-            printf("3. WIA\n");
-            printf("4. 고장난 엔진\n");
-        }
+            print_engine_option();
         else if (step == brakeSystem_Q)
-        {
-            printf(CLEAR_SCREEN);
-            printf("어떤 제동장치를 선택할까요?\n");
-            printf("0. 뒤로가기\n");
-            printf("1. MANDO\n");
-            printf("2. CONTINENTAL\n");
-            printf("3. BOSCH\n");
-        }
+            print_brake_system_option();
         else if (step == SteeringSystem_Q)
-        {
-            printf(CLEAR_SCREEN);
-            printf("어떤 조향장치를 선택할까요?\n");
-            printf("0. 뒤로가기\n");
-            printf("1. BOSCH\n");
-            printf("2. MOBIS\n");
-        }
+            print_steer_system_option();
         else if (step == Run_Test)
-        {
-            printf(CLEAR_SCREEN);
-            printf("멋진 차량이 완성되었습니다.\n");
-            printf("어떤 동작을 할까요?\n");
-            printf("0. 처음 화면으로 돌아가기\n");
-            printf("1. RUN\n");
-            printf("2. Test\n");
-        }
+            print_run_test();
+
         printf("===============================\n");
 
         printf("INPUT > ");
